5dc5e4984c0180231fd9d445da1afaeea5f965f7
Shahriori <ShahTheShah@ya.ru>
2023-03-19 20:50:42 +0300

    Update

--- TypeTask/functions.py
+++ TypeTask/functions.py
@@ -1,141 +1,213 @@
-import math
 from numpy import *
+import math
 import scipy
 
-def forward_elimination(A, b, n):
-    for row in range(0, n-1):
-        for i in range(row+1, n):
-            factor = A[i,row] / A[row,row]
-            for j in range(row, n):
-                A[i,j] = A[i,j] - factor * A[row,j]
+import colorama
+from colorama import Fore, Back, Style, init as coloramaInit
 
-            b[i] = b[i] - factor * b[row]
-        str1 = 'A = '
-        str2 = ' и B = '
-        string = ''
-        for i in range(0, len(A)):
-            if i == 0 or i == 2:
-                string += '    '
-                string += str(A[i])
-                string += '       '
-                string += str(b[i])
-                string += '\n'
-            elif i == 1:
-                string += str1
-                string += str(A[i])
-                string += str2
-                string += str(b[i])
-                string += '\n'
-        # print(string)
-    return A, b
+coloramaInit(autoreset=True)
 
-def back_substitution(a, b, n):
-    x = zeros((n,1))
-    x[n-1] = b[n-1] / a[n-1, n-1]
-    for row in range(n-2, -1, -1):
-        sums = b[row]
-        for j in range(row+1, n):
-            sums = sums - a[row,j] * x[j]
-        x[row] = sums / a[row,row]
-    return x
 
-def gauss(A, b):
-    n = A.shape[0]
-    if any(diag(A)==0):
+def forward_elimination(Matrix_A, Matrix_B, matrix_shape, output):
+
+    # ?------------------------ Calculation Block ------------------------
+    for row in range(0, matrix_shape - 1):
+        for i in range(row + 1, matrix_shape):
+            factor = Matrix_A[i, row] / Matrix_A[row, row]
+            for j in range(row, matrix_shape):
+                Matrix_A[i, j] = Matrix_A[i, j] - factor * Matrix_A[row, j]
+
+            Matrix_B[i] = Matrix_B[i] - factor * Matrix_B[row]
+    # ?------------------------ Calculation Block ------------------------
+
+    # ?-------------------------- Output Block ---------------------------
+        if output:
+            string_matrix = 'Equation = '
+            string = ''
+            for i in range(0, len(Matrix_A)):
+                if i == 0 or i == 2:
+                    string += '\033[0m           '
+                    string += f'\033[33m\033[1m[ {str(Matrix_A[i])[1:-1]}'
+                    string += f' | x{i + 1} ]\033[0m   '
+                    string += f'\033[33m\033[1m{str(Matrix_B[i])}'
+                    string += '\n'
+                elif i == 1:
+                    string += string_matrix
+                    string += f'\033[33m\033[1m[ {str(Matrix_A[i])[1:-1]}'
+                    string += f' | x{i + 1} ]\033[33m\033[1m = '
+                    string += f'\033[33m\033[1m{str(Matrix_B[i])}'
+                    string += '\n'
+            print(string)
+    # ?-------------------------- Output Block ---------------------------
+
+    return Matrix_A, Matrix_B
+
+
+def back_substitution(Matrix_A, Matrix_B, matrix_shape):
+    # ?------------------------ Calculation Block ------------------------
+    matrix_zeros = zeros((matrix_shape, 1))
+    matrix_zeros[matrix_shape - 1] = Matrix_B[matrix_shape - 1] / Matrix_A[matrix_shape - 1, matrix_shape - 1]
+    for row in range(matrix_shape - 2, -1, -1):
+        sums = Matrix_B[row]
+        for j in range(row + 1, matrix_shape):
+            sums = sums - Matrix_A[row, j] * matrix_zeros[j]
+        matrix_zeros[row] = sums / Matrix_A[row, row]
+    # ?------------------------ Calculation Block ------------------------
+
+    return matrix_zeros
+
+
+def Gauss_Method(Matrix_A, Matrix_B, output = False):
+    if output:
+        print('\n\033[31m\033[1m\033[3m------------ Start Gauss method -------------')
+        print('\n')
+
+
+    # ?------------------------ Calculation Block ------------------------
+    matrix_shape = Matrix_A.shape[0]
+    if any(diag(Matrix_A) == 0):
         raise ZeroDivisionError(('Division by zero will occur; '
-                                  'pivoting currently not supported'))
+                                 'pivoting currently not supported'))
 
-    A, b = forward_elimination(A, b, n)
-    return back_substitution(A, b, n)
+    Matrix_A, Matrix_B = forward_elimination(Matrix_A, Matrix_B, matrix_shape, output)
+    roots = back_substitution(Matrix_A, Matrix_B, matrix_shape)
+    # ?------------------------ Calculation Block ------------------------
 
-def crout(a, b):
-    cout = 0
-    m, n = a.shape
-    if (m !=n ):
-        print("Crout cannot be used.")
+
+
+    if output:
+    # ?-------------------------- Output Block ---------------------------
+        string_matrix = 'Answer   = '
+        string = ''
+        rootsVar = ['x1', 'x2', 'x3']
+        for i in range(0, len(Matrix_A)):
+            if i == 0 or i == 2:
+                string += '\033[0m           '
+                string += f'\033[32m\033[1m[ {rootsVar[i]} = {"%.5f" % roots[i]} ]\n'
+            elif i == 1:
+                string += string_matrix
+                string += f'\033[32m\033[1m[ {rootsVar[i]} = {"%.5f" % roots[i]} ]\n'
+        print(string)
+        print('\n\033[31m\033[1m\033[3m------------- End Gauss method --------------')
+    # ?-------------------------- Output Block ---------------------------
     else:
-        l = zeros((n,n))
-        u = zeros((n,n))
-        s1 = 0
-        s2 = 0
+        return roots
 
-        for i in range(n):
-           l[i][0] = a[i][0]
-           u[i][i] = 1
-        for j in range(1, n):
-            u[0][j] = a[0][j] / l[0][0]
-        for k in range(1, n):
-            for i in range(k, n):
-                for r in range(k): s1 += l[i][r] * u[r][k]
-                l[i][k] = a[i][k] - s1
-                s1 = 0
-            for j in range(k+1, n):
-                for r in range(k): s2 += l[k][r] * u[r][j]
-                u[k][j] = (a[k][j] - s2) / l[k][k]
-                s2 = 0
 
-        y = zeros(n)
-        s3 = 0
-        y[0] = b[0] / l[0][0]
-        for k in range(1, n):
+def Crout_Method(Matrix_A, Matrix_B, output = False):
+    if output:
+        print('\n\033[31m\033[1m\033[3m------------ Start Crout method -------------')
+        print('\n')
+
+    # ?------------------------ Calculation Block ------------------------
+    cout = 0
+    Count_Rows_Matrix, Count_Columns_Matrix = Matrix_A.shape
+    if (Count_Rows_Matrix != Count_Columns_Matrix):
+        if output:
+            print('The matrix is not homogeneous')
+        else:
+            return 0
+
+    L_Matrix = zeros((Count_Columns_Matrix, Count_Columns_Matrix))
+    U_Matrix = zeros((Count_Columns_Matrix, Count_Columns_Matrix))
+    Time_Matrix = 0
+
+    for i in range(Count_Columns_Matrix):
+        L_Matrix[i][0] = Matrix_A[i][0]
+        U_Matrix[i][i] = 1
+    for j in range(1, Count_Columns_Matrix):
+        U_Matrix[0][j] = Matrix_A[0][j] / L_Matrix[0][0]
+    for k in range(0, Count_Columns_Matrix):
+        for i in range(k, Count_Columns_Matrix):
             for r in range(k):
-                s3 += l[k][r] * y[r]
-            y[k] = (b[k]-s3) / l[k][k]
-            s3 = 0
+                Time_Matrix += L_Matrix[i][r] * U_Matrix[r][k]
+            L_Matrix[i][k] = Matrix_A[i][k] - Time_Matrix
+            Time_Matrix = 0
+        for j in range(k + 1, Count_Columns_Matrix):
+            for r in range(k):
+                Time_Matrix += L_Matrix[k][r] * U_Matrix[r][j]
+            U_Matrix[k][j] = (Matrix_A[k][j] - Time_Matrix) / L_Matrix[k][k]
+            Time_Matrix = 0
 
-        x = zeros(n)
-        s4 = 0
-        x[n-1] = y[n-1]
-        for k in range(n-2, -1, -1):
-            for r in range(k+1, n):
-                s4 += u[k][r] * x[r]
-            x[k] = y[k] - s4
-            s4 = 0
 
-        roots = {'x': list(x)}
+    y = zeros(Count_Columns_Matrix)
+    y[0] = Matrix_B[0] / L_Matrix[0][0]
+    for k in range(1, Count_Columns_Matrix):
+        for r in range(k):
+            Time_Matrix += L_Matrix[k][r] * y[r]
+        y[k] = (Matrix_B[k] - Time_Matrix) / L_Matrix[k][k]
+        print(y)
+        Time_Matrix = 0
+
+    x = zeros(Count_Columns_Matrix)
+    x[Count_Columns_Matrix - 1] = y[Count_Columns_Matrix - 1]
+    print(x)
+    for k in range(Count_Columns_Matrix - 2, -1, -1):
+        for r in range(k + 1, Count_Columns_Matrix):
+            Time_Matrix += U_Matrix[k][r] * x[r]
+        x[k] = y[k] - Time_Matrix
+        Time_Matrix = 0
+    print(x)
+
+    roots = {}
+    for i in range(Count_Columns_Matrix):
+        roots["x" + str(i + 1)] = float("%.5f" % x[i])
+    # print(roots)
+    # ?------------------------ Calculation Block ------------------------
+
+
+    print('\n\033[31m\033[1m\033[3m------------- End Crout method --------------')
+    return [roots, L_Matrix, U_Matrix]
+
+
+def jacobi(Matrix_A, Matrix_B, iteration = 2, Zeros_Matrix = None, output = False):
+    if output:
+        print('\n\033[31m\033[1m\033[3m------------ Start Jacobi method -------------')
+        print('\n')
+
+    if Zeros_Matrix is None:
+        Zeros_Matrix = zeros(len(Matrix_A))
+
+    D = diag(Matrix_A)
+    R = Matrix_A - diagflat(D)
+
+    for i in range(iteration):
+        Zeros_Matrix = (Matrix_B - dot(R, Zeros_Matrix)) / D
+    print(Zeros_Matrix)
+    print('\n\033[31m\033[1m\033[3m------------- End Jacobi method --------------')
+    return Zeros_Matrix
+
+
+def gauss_saidel(Matrix_A, Matrix_B, iteration=2, accuracy=1e-6):
+    n = len(Matrix_A)
+
+    btol = linalg.norm(Matrix_B) * accuracy
+
+    x0 = zeros(n)
+    k = 0
+    isActive = False
+    x1 = empty(n)
+
+    while not (isActive) and k < iteration:
+        print(f'Начало итерации при k = {k}')
+        print('------------------------------------')
+        x1 = zeros(n)
         for i in range(n):
-            roots["x" + str(i + 1)] = x[i]
-        return [roots, l, u]
+            x1[i] = (Matrix_B[i] - dot(Matrix_A[i, 0:i], x1[0:i]) - dot(Matrix_A[i, i + 1:n], x0[i + 1:n])) / Matrix_A[i, i]
+            print("x1  = ", x1)
 
-def jacobi(A,b,N=2,x=None):
-    if x is None:
-        x = zeros(len(A[0]))
+        r = Matrix_B - dot(Matrix_A, x1)
+        isActive = (linalg.norm(r) < btol) and (linalg.norm(x1-x0) < accuracy)
+        print(f'\n')
+        print("x0  = ", x0)
+        print("btol = %e;\nla.norm(r) = %e;\ntol = %e;\nla.norm(x1-x0) = %e;\nisActive = %s " % (btol, linalg.norm(r), accuracy, linalg.norm(x1-x0), isActive))
+        x0 = x1
+        print("x0  = ", x0, end='\n')
+        print('------------------------------------')
+        print("Окончание итерации \n\n")
+        k = k + 1
 
-    D = diag(A)
-    R = A - diagflat(D)
+    if not (isActive):
+        print(f'Не сходится за {iteration} итерации.')
 
-    for i in range(N):
-        x = (b - dot(R,x)) / D
-    return x
-
-def gauss_saidel(A,b, N=2, esp=1e-6):
-        n = len(A)
-
-        btol = linalg.norm(b) * esp
-
-        x0   = zeros(n)
-        k    = 0 ;
-        isActive = False
-        x1   = empty(n)
-
-        while not(isActive) and k < N:
-            print (f'Начало цикла при k = {k}')
-            x1 = zeros(n)
-            for i in range(n):          # rows of A
-               x1[i] = ( b[i] - dot(A[i,0:i], x1[0:i]) - dot(A[i,i+1:n], x0[i+1:n]) ) / A[i,i]
-               print("x1  = ", x1)
-
-            r = b - dot(A,x1)
-            isActive = (linalg.norm(r) < btol) and (linalg.norm(x1-x0) < esp)
-            print(f'\n')
-            print("x0  = ", x0)
-            print("btol = %e;\nla.norm(r) = %e;\ntol = %e;\nla.norm(x1-x0) = %e;\nisActive = %s " % (btol, linalg.norm(r), esp, linalg.norm(x1-x0), isActive))
-            x0   = x1
-            print("x0  = ", x0, end='\n')
-            print("Окончание цикла \n\n")
-            k    = k + 1
-
-        if not(isActive): # or if k >= N
-            print(f'Не сходится за {N} итерации.')
-
-        return x1
+    return x1
--- TypeTask/main2.py
+++ TypeTask/main2.py
@@ -1,16 +1,14 @@
-import math
 from numpy import *
-import scipy
 from functions import *
 
 # Выставляем количество знаков после запятой в матрице
 set_printoptions(precision=3)
 
 # Получаем вариант
-N = int(input("Ваш вариант: "))
+N = int(input("\n\033[34m\033[1mВаш вариант: "))
 
 MATRIX_A = array([
-    [12 * N, N, N + 2],
+    [10 * N, N, N + 2],
     [N, 9 * N, 3 * N],
     [2 * N, 3 * N, 11 * N],
 ])
@@ -18,8 +16,7 @@ MATRIX_B = array([
     [2 * N], [N], [3 * N]
 ])
 
-ROOTS         = gauss (MATRIX_A, MATRIX_B)
-[roots, L, U] = crout (MATRIX_A, MATRIX_B)
-JACOBI        = jacobi(MATRIX_A, MATRIX_B)
-GAUSS_SAIDEL = gauss_saidel(MATRIX_A, MATRIX_B)
-
+Gauss_Method(MATRIX_A, MATRIX_B, output = True)
+Crout_Method(MATRIX_A, MATRIX_B, output = True)
+jacobi(MATRIX_A, MATRIX_B, iteration = 2, output = True)
+# GAUSS_SAIDEL  = gauss_saidel(MATRIX_A, MATRIX_B, iteration=2, accuracy=1e-6)
